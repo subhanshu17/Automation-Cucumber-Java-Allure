a7d0f841f334dcdd93d66dae61b2641ea982af66
17sub <17sub@home>
2025-08-21 11:44:20 +0530

    fix

--- src/main/java/com/framework/utils/ReportGenerator.java
+++ src/main/java/com/framework/utils/ReportGenerator.java
@@ -8,14 +8,14 @@ public class ReportGenerator {
 
     @AfterSuite
     public void generateAllureReport() {
-        try {
-            String command = "allure generate target/allure-results -o target/allure-report --clean";
-            Process process = Runtime.getRuntime().exec(command);
-            process.waitFor();
-            System.out.println("✅ Allure Report generated at: target/allure-report/index.html");
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-            System.out.println("⚠️ Failed to generate Allure Report automatically.");
-        }
+//        try {
+//            String command = "allure generate target/allure-results -o target/allure-report --clean";
+//            Process process = Runtime.getRuntime().exec(command);
+//            process.waitFor();
+//            System.out.println("✅ Allure Report generated at: target/allure-report/index.html");
+//        } catch (IOException | InterruptedException e) {
+//            e.printStackTrace();
+//            System.out.println("⚠️ Failed to generate Allure Report automatically.");
+//        }
     }
 }
